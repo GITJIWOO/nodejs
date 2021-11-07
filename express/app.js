75b6816824e1695ad5b06c91255e6661ade9a68e
PYOJITAE <kjw011231@gmail.com>
2021-11-08 00:29:18 +0900

    express basic

--- express/app.js
+++ express/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000); // 전역 변수
 
-app.get('/app', (req, res) => { // localhost:3000/app의 주소로 접속 가능
+app.get('/', (req, res) => { // localhost:3000/app의 주소로 접속 가능
+    res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.get('/app', (req, res) => {
     res.send('hello express');
 });
 
