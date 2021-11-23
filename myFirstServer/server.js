5aeb514de1e46f788c0005d8ef9ebae73a38b49d
PYOJITAE <kjw011231@gmail.com>
2021-11-23 23:15:00 +0900

    express

--- myFirstServer/server.js
+++ myFirstServer/server.js
@@ -3,8 +3,12 @@ const path = require('path');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const ejs = require('ejs');
 
 const router = express();
+
+router.set('view engine', 'ejs'); //'ejs'탬플릿을 엔진으로 한다.
+router.set('views', path.join(__dirname, './views'));
 
 const getJson = () => 
     JSON.stringify({
@@ -16,12 +20,19 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/main', (req, res) => {
-    res.sendFile(path.join(__dirname, './main.html'));
+    res.sendFile(path.join(__dirname, './views/main.html'));
 });
 
 router.post('/login', (req, res) => {
     const id = req.body.id;
-    console.log(id);
+    res.render('welcome', {
+        title: 'Login',
+        id: id,
+    });
+});
+
+router.get('/welcome', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/welcome.html'));
 });
 
 router.use((err, req, res, next) => {
